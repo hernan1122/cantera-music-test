1deb56ce9273ff1409d3f99d47e794907e9d6969
Marlene <marlene.nogara@revalcodesolution.com>
2023-04-21 16:41:24 -0300

    Cambios hola soy marlene

--- src/components/PlayerHeader.jsx
+++ src/components/PlayerHeader.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import '../styles/PlayerHeader.css'
+import React from "react";
+import "../styles/PlayerHeader.css";
 
-//Icons 
-import {BsArrowLeftShort} from 'react-icons/bs'
-import{BsMusicNoteList} from 'react-icons/bs'
+//Icons
+import { BsArrowLeftShort } from "react-icons/bs";
+import { BsMusicNoteList } from "react-icons/bs";
 function PlayerHeader(props) {
   return (
-    <div className='PlayerHeader'>
-      <BsArrowLeftShort className='PlayerHeader-Icon'/>
-      <h2>Reproduciendo</h2>
-      <BsMusicNoteList className='PlayerHeader-Icon' />
+    <div className="PlayerHeader">
+      <BsArrowLeftShort className="PlayerHeader-Icon" />
+      <h2>Hola Soy Marlene</h2>
+      <BsMusicNoteList className="PlayerHeader-Icon" />
     </div>
   );
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from '../Home';
-import { Player } from '../components/Player';
-import { CategoryMusic } from '../components/CategoryMusic';
-import { CategoryPodcast } from '../components/CategoryPodcast';
-import { CategoryAudioBooks } from '../components/CategoryAudioBooks';
-import { NotFound } from '../components/NotFound';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Player } from "../components/Player";
+import { CategoryMusic } from "../components/CategoryMusic";
+import { CategoryPodcast } from "../components/CategoryPodcast";
+import { CategoryAudioBooks } from "../components/CategoryAudioBooks";
+import { NotFound } from "../components/NotFound";
+import PlayerSlider from "../components/PlayerSlider";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/player' element={<Player />} />
-        <Route path='/category/music' element={<CategoryMusic />} />
-        <Route path='/category/podcast' element={<CategoryPodcast />} />
-        <Route path='/category/audiobooks' element={<CategoryAudioBooks />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/category/music" element={<CategoryMusic />} />
+        <Route path="/category/podcast" element={<CategoryPodcast />} />
+        <Route path="/category/audiobooks" element={<CategoryAudioBooks />} />
+        <Route path="/PlayerSlider" element={<PlayerSlider />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
