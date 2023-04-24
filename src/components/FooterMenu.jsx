db68817b0f10d3cb3db6105f6481da7bb56a1f74
Axel Torrico <torricohernan2@gmail.com>
2023-04-24 14:56:06 -0300

    prueba de commit

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -12,15 +12,15 @@ export function FooterMenu() {
     <div className='FooterMenu'>
       <Link className='FooterMenu-link' to='/player'>
         <BiCategory className='FooterMenu-icons' />
-        <h5>Categorías</h5>
+        <h6>Categorías</h6>
       </Link>
       <Link className='FooterMenu-link' to='/'>
         <HiHome className='FooterMenu-icons' />
-        <h5>Inicio</h5>
+        <h6>Inicio</h6>
       </Link>
       <Link className='FooterMenu-link' to='/'>
         <BiAddToQueue className='FooterMenu-icons' />
-        <h5>Agregar</h5>
+        <h6>Agregar</h6>
       </Link>
     </div>
   );
