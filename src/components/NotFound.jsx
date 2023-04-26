a7131c5614120f6c28a2fa4552bb2da0a5ff9795
fspind71 <fspind@gmail.com>
2023-04-26 10:13:03 -0300

    creacion del componente NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import '../styles/NotFound.css'
 
-export function NotFound() {
-  return (
-    <div>
-      errorrrrrr
-    </div>
-  );
+import logoApp from '../images/cat.png';
+import imgError from '../images/pexels-lucas-pezeta-2930204.png'
+
+export function NotFound(){
+  return(
+    
+    <div className='Header'>
+      <div className="Header-title">
+        <div className="Header-content-title">
+          <img className="Header-logoApp" src={logoApp} alt="" />
+            <h1>Cantera <span>Music</span></h1>      
+        </div>
+      </div>
+      <main>
+      <div className="content-imgError">
+          <img className="imgError" src={imgError} alt="gatoBurlon" />
+          <h1 className="Error-LineText">Oops!<br/>Algo salió Mal!!!</h1>          
+          <button className="error-volver">Volver</button>         
+      </div>
+      </main>      
+    </div>    
+  )
 }
