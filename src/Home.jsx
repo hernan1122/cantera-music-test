c3754b589b945d09c5fcc7462ab28624389c7f96
Axel Torrico <torricohernan2@gmail.com>
2023-04-28 15:42:14 -0300

    .......2

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ export function Home() {
           <Slider />
           <Slider />
           <Slider />
-          <div className="Song-container-sliderSong-space">..............</div>
+          <div className="Song-container-sliderSong-space">...........</div>
         </div>
       </div>
       <Categories />
